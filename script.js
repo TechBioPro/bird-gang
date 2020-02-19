d0407935781f3a586949aa14b724930112520f58
Real Name <61215521+TechBioPro@users.noreply.github.com>
2020-02-19 09:07:40 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//var user = document.getElementById("user").value;  
+function check(){
+var pass = document.getElementById("pass").value; 
+if (pass == "birds&seeds47") {
+  window.location.assign('4917614421493898.html');
+} else if (pass == "nickleader39") {
+  window.location.assign("702323876684805800.html");
+} else {
+  document.getElementById("error").innerHTML = "NO SEEDS FOR YOU!";
+}
+}
+function forgot(){
+  var person = prompt("Please enter the code", "IDK");
+  if (person==396425){ 
+    alert("That password is birds&seeds47.");
+  }
+}
